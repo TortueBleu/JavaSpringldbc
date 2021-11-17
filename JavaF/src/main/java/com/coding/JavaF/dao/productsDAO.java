786887184eb659f2ab5fec38a641b85323f8d942
Anais <anais.hammouche@edu.itescia.fr>
2021-11-17 15:59:14 +0100

    controller

--- JavaF/src/main/java/com/coding/JavaF/dao/productsDAO.java
+++ JavaF/src/main/java/com/coding/JavaF/dao/productsDAO.java
@@ -1,4 +1,19 @@
 package com.coding.JavaF.dao;
 
+import jdk.jfr.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public class productsDAO {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public List<products> listAll() {
+
+    }
 }
