79d46804f312707a746e2de467abf17e5d2b2525
TortueBleu <admin@MacBook-Pro-de-admin.local>
2021-11-18 09:50:55 +0100

    ajourt d'une acollade

--- JavaF/src/main/java/com/coding/JavaF/CategoryController.java
+++ JavaF/src/main/java/com/coding/JavaF/CategoryController.java
@@ -13,40 +13,38 @@ import java.util.List;
 public class CategoryController {
 
 
-   @Autowired
+    @Autowired
     private CategoryDAO categoryService;
 
- //   @RequestMapping("/category")
-  //  public String indexCategory(Model model){
-   //    model.addAttribute("listCategory", CategoryDAO.listAll());
+    //   @RequestMapping("/category")
+    //  public String indexCategory(Model model){
+    //    model.addAttribute("listCategory", CategoryDAO.listAll());
     //    return "indexCategory";
 
-  //  }
+    //  }
 
 
     @RequestMapping("/categories")
-    public String indexCategory(Model model){
+    public String indexCategory(Model model) {
         model.addAttribute("listCategory", categoryService.listAll());
         return "indexCategory";
 
 
- //   @RequestMapping("/category")
+        //   @RequestMapping("/category")
 //    public List<Category> getAllCategories() {
- //       return CategoryDAO.listAll();
- //   }
+        //       return CategoryDAO.listAll();
+        //   }
 
 
-  //  @PostMapping("/add")
- //   public @ResponseBody String addNewCategories(@RequestParam String id, @RequestParam String name){
+        //  @PostMapping("/add")
+        //   public @ResponseBody String addNewCategories(@RequestParam String id, @RequestParam String name){
 
-  //      Category cat = new Category();
-  //      cat.setId(id);
-  //      cat.setName(name);
-  //      CategoryDAO.save(cat);
-   //     return "SET OK";
-
-
+        //      Category cat = new Category();
+        //      cat.setId(id);
+        //      cat.setName(name);
+        //      CategoryDAO.save(cat);
+        //     return "SET OK";
 
 
+    }
 }
-
